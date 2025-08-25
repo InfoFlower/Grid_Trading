15ed918412169f007a9b5df83b2437b090603111
luludocteur <luludocteurcrypt@gmail.com>
2025-08-25 18:55:30 +0200

    test dag

--- src/DEC/ASTRO/dags/premierdag.py
+++ src/DEC/ASTRO/dags/premierdag.py
@@ -1,30 +1,28 @@
-from datetime import datetime
-
 from airflow.decorators import dag
+from pendulum import datetime 
+from airflow.operators.empty import EmptyOperator
 
-from astro import sql as aql
-from astro.sql.table import Table
 
-connection_id = 'postgresql_GTBDD'
-
-@aql.transform
-def select(input_table : Table):
-    return 'SELECT * FROM {{input_table}}'
-
-@dag(schedule = None,
-     start_date = datetime(2024,7,25),
-     catchup = False
+@dag(
+    start_date=datetime(2023, 1, 1),
+    schedule="@daily",
+    catchup=False,
 )
-def lol():
+def my_dag():
 
-    mdr = select(input_table="ORDERI", conn_id = connection_id)
+    t1 = EmptyOperator(task_id="t1")
 
-dag = lol()
+
+dag_object = my_dag()
 
 if __name__ == "__main__":
-    conn_path = "include/connections.yml"
+    conn_path = "include/connections.yaml"
+    #variables_path = "variables.yaml"
+    #my_conf_var = 23
 
-    dag.test(
-    execution_date = datetime(2026, 7, 24),
-    conn_file_path = conn_path
+    dag_object.test(
+        execution_date=datetime(2023, 1, 29),
+        conn_file_path=conn_path,
+        #variable_file_path=variables_path,
+        #run_conf={"my_conf_var": my_conf_var},
     )
