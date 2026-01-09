de77111f201e385454a8b22f94fbdb56305b5e06
luludocteur <luludocteurcrypt@gmail.com>
2026-01-09 20:55:06 +0100

    create distibution tables

--- src/DEC/AIRFLOW/dbt/tb_dbt/models/dmbtc/c_market_data.sql
+++ src/DEC/AIRFLOW/dbt/tb_dbt/models/dmbtc/c_market_data.sql
@@ -1,4 +1,5 @@
-select *
+select 'default_ BTCUSDT' as symbol 
+    , *
     , '{{ run_started_at }}' as execution_timestamp
     , '{{ invocation_id }}' as invocation_id
 from {{ source('dmbte', 'e_market_data') }}
